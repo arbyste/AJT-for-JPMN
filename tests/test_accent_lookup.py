9ad378b763c97fa123fe7a0d4c5821c14e84d007
Ren Tatsumoto <tatsu@autistici.org>
2024-07-27 01:48:07 +0300

    update tests

--- tests/test_accent_lookup.py
+++ tests/test_accent_lookup.py
@@ -1,10 +1,15 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+import pathlib
+import re
 from collections.abc import Sequence
 
 import pytest
 
+from japanese.furigana.gen_furigana import FuriganaGen
+from japanese.helpers.profiles import ColorCodePitchFormat
 from japanese.helpers.sqlite3_buddy import Sqlite3Buddy
+from japanese.helpers.tokens import HTML_AND_MEDIA_REGEX, clean_furigana
 from japanese.mecab_controller import MecabController, to_katakana
 from japanese.pitch_accents.acc_dict_mgr_2 import AccentDictManager2
 from japanese.pitch_accents.accent_lookup import AccentLookup
@@ -24,29 +29,34 @@ except ImportError:
             a = b
 
 
+def furigana_to_word_seq(furigana_output: str) -> list[str]:
+    return [part for part in re.split(HTML_AND_MEDIA_REGEX, clean_furigana(furigana_output)) if part]
+
+
 class TestAccDictLookup:
     @pytest.fixture(scope="class")
-    def acc_dict_mgr(self, tmp_sqlite3_db_path, tmp_upd_file, tmp_user_accents_file) -> AccentDictManager2:
+    def acc_dict_mgr(
+        self, tmp_sqlite3_db_path: pathlib.Path, tmp_upd_file: pathlib.Path, tmp_user_accents_file: pathlib.Path
+    ) -> AccentDictManager2:
         acc_dict = AccentDictManager2(tmp_sqlite3_db_path, tmp_upd_file, tmp_user_accents_file)
         acc_dict.ensure_dict_ready_on_main()
         return acc_dict
 
     @pytest.fixture(scope="class")
-    def lookup(self, no_anki_config: NoAnkiConfigView, acc_dict_mgr: AccentDictManager2) -> AccentLookup:
+    def lookup(
+        self,
+        no_anki_config: NoAnkiConfigView,
+    ) -> AccentLookup:
         cfg = no_anki_config
         mecab = MecabController(verbose=False, cache_max_size=cfg.cache_lookups)
         lookup = AccentLookup(cfg, mecab)
         return lookup
 
-    @pytest.mark.parametrize(
-        "test_input, expected",
-        [("聞かせて戻って", ("聞く", "戻る")), ("経緯と国境", ("経緯", "国境"))],
-    )
-    def test_accent_lookup(self, tmp_sqlite3_db_path, lookup: AccentLookup, test_input, expected) -> None:
-        with Sqlite3Buddy(tmp_sqlite3_db_path) as db:
-            result = lookup.with_new_buddy(db).get_pronunciations(test_input)
-        for item in expected:
-            assert item in result
+    @pytest.fixture(scope="class")
+    def fgen(self, no_anki_config: NoAnkiConfigView, lookup: AccentLookup) -> FuriganaGen:
+        cfg = no_anki_config
+        fgen = FuriganaGen(cfg, lookup)
+        return fgen
 
     @pytest.mark.parametrize(
         "word, order",
@@ -67,3 +77,49 @@ class TestAccDictLookup:
         assert acc_dict_mgr.is_ready()
         assert not acc_dict_mgr.lookup("missing")
         assert not acc_dict_mgr.lookup("")
+
+    @pytest.mark.parametrize(
+        "test_input, expected",
+        [("聞かせて戻って", ("聞く", "戻る")), ("経緯と国境", ("経緯", "国境"))],
+    )
+    def test_accent_lookup(
+        self, tmp_sqlite3_db_path: pathlib.Path, lookup: AccentLookup, test_input: str, expected: Sequence[str]
+    ) -> None:
+        with Sqlite3Buddy(tmp_sqlite3_db_path) as db:
+            result = lookup.with_new_buddy(db).get_pronunciations(test_input)
+        for item in expected:
+            assert item in result
+
+    @pytest.mark.parametrize(
+        "sentence, expected",
+        [
+            ("随分思い切ったなぁとか思ってたけど", ["随分", "思い切った", "なぁとか", "思ってた", "けど"]),
+            ("倒しただけだろ", ["倒した", "だけだろ"]),
+            ("弄ばれてしまった", ["弄ばれて", "しまった"]),
+            ("熱くなった", ["熱く", "なった"]),
+            ("分かりませんが", ["分かりません", "が"]),
+            ("疑わなかったの", ["疑わなかった", "の"]),
+            ("赤くなかった", ["赤くなかった"]),
+            ("長けておる", ["長けて", "おる"]),
+            (
+                "でも最近はそんなに忙しくなかったんでしょ",
+                ["でも", "最近", "は", "そんなに", "忙しくなかった", "んでしょ"],
+            ),
+            ("借金も抱えてたじゃない", ["借金", "も", "抱えてた", "じゃない"]),
+            ("あるでしょうし", ["ある", "でしょうし"]),
+            ("思ったんでしょう", ["思った", "んでしょう"]),
+            ("出たな", ["出た", "な"]),
+            ("ボーイフレンドを見つけたらしい", ["ボーイフレンド", "を", "見つけた", "らしい"]),
+            ("死なないでくれ", ["死なないで", "くれ"]),
+            ("僕はそれに立ち会ったにすぎません", ["僕", "は", "それ", "に", "立ち会った", "に", "すぎません"]),
+        ],
+    )
+    def test_attach_rules(
+        self, tmp_sqlite3_db_path: pathlib.Path, fgen: FuriganaGen, sentence: str, expected: Sequence[str]
+    ) -> None:
+        with Sqlite3Buddy(tmp_sqlite3_db_path) as db:
+            text = fgen.with_new_buddy(db).generate_furigana(
+                sentence,
+                output_format=ColorCodePitchFormat.attributes,
+            )
+            assert furigana_to_word_seq(text) == expected
